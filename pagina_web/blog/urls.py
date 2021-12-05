e32218848683f3b11713c0a1d3e24b1fe269e9a2
JuanCobas <juanycobas@gmail.com>
2021-12-05 16:35:53 -0300

    Posts y usuarios terminados. Faltan permisos para postear, comentarios y buscador

--- pagina_web/blog/urls.py
+++ pagina_web/blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='home-blog'),
+    path('post/<str:username>', UserPostListView.as_view(), name='user-post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    
+
 
 ]
